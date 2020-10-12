1fc78c4504d8b1376a480426a35b83ffbc40f3f9
AmjadWebDev <amjadwebdev@gmail.com>
2020-10-12 20:03:27 +0200

    add alert msg and router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/navbar";
 import Users from "./components/users/users";
 import Search from "./components/users/search";
+import Alert from "./components/layout/alert";
+import About from "./components/pages/about";
 import axios from "axios";
 
 class App extends React.Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
   // async componentDidMount() {
   //   this.setState({ loading: true });
@@ -28,22 +32,44 @@ class App extends React.Component {
     });
   };
 
+  //clear users from starte
   clearUsers = () =>
     this.setState({
       users: [],
       loading: false,
     });
 
+  //set alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+
   render() {
     const { users, loading } = this.state;
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false} />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false} setAlert={this.setAlert} />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/users/search.js
+++ src/components/users/search.js
@@ -10,8 +10,12 @@ class Search extends Component {
   };
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: "" });
+    if (this.state.text === "") {
+      this.props.setAlert("please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
   };
 
   render() {
