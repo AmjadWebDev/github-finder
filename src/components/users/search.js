c61e173e6903968f69a18a9b820fd5e4876e7066
AmjadWebDev <amjadwebdev@gmail.com>
2020-10-12 14:31:22 +0200

    add clear btn

--- src/components/users/search.js
+++ src/components/users/search.js
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+  state = {
+    text: "",
+  };
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: "" });
+  };
+
   render() {
     return (
       <div>
-        <form className="form"></form>
+        <form onSubmit={this.onSubmit} className="form">
+          <input type="text" name="text" value={this.state.text} onChange={this.onChange} placeholder="Search Users..." />
+          <input type="submit" value="Search" className="btn btn-dark btn-block" />
+        </form>
+        {this.props.showClear && (
+          <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
