eab727849d83dcbff7cba6cbb15c768560bff7fc
AmjadWebDev <amjadwebdev@gmail.com>
2020-10-26 10:32:12 +0100

    transfer search & user to hooks

--- src/components/users/search.js
+++ src/components/users/search.js
@@ -1,38 +1,34 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Search extends Component {
-  state = {
-    text: "",
-  };
+const Search = ({ setAlert, searchUsers, clearUsers, showClear }) => {
+  const [text, setText] = useState("");
 
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const onChange = (e) => {
+    setText(e.target.value);
   };
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (this.state.text === "") {
-      this.props.setAlert("please enter something", "light");
+    if (text === "") {
+      setAlert("please enter something", "light");
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: "" });
+      searchUsers(text);
+      setText("");
     }
   };
 
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className="form">
-          <input type="text" name="text" value={this.state.text} onChange={this.onChange} placeholder="Search Users..." />
-          <input type="submit" value="Search" className="btn btn-dark btn-block" />
-        </form>
-        {this.props.showClear && (
-          <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input type="text" name="text" value={text} onChange={onChange} placeholder="Search Users..." />
+        <input type="submit" value="Search" className="btn btn-dark btn-block" />
+      </form>
+      {showClear && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default Search;
